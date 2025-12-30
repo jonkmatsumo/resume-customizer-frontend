cd404991c7d045da304f231437ae81c4035ed6b3
jonkmatsumo <jonkmatsumo@gmail.com>
2025-12-29 20:16:13 -0800

    finish setting up husky and use provideAnimations hook

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,11 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+// TODO: provideAnimations is deprecated in Angular 21 in favor of native CSS animations.
+// However, Angular Material still requires it. Remove this once Angular Material migrates
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
-    provideAnimationsAsync(),
-  ],
+  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes), provideAnimations()],
 };
